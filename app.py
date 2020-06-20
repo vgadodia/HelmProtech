dbc62f010a553c3ce7f66a0a16762f28d64a5ac1
Veer Gadodia <veergadodia24@gmail.com>
2020-06-20 16:21:41 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,57 @@
+'''
+I'm trying to use this: https://flask-pymongo.readthedocs.io/en/latest/
+'''
+
+import pandas as pd
+from flask import Flask, jsonify, request, send_file, render_template, redirect, url_for
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+from werkzeug.utils import secure_filename
+from datetime import datetime
+import os
+import pathlib
+import hashlib
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def getupload():
+    if request.method == "POST":
+        memory = request.files['memory']
+        if memory.filename == "":
+            return render_template('upload.html', errorMessage="You must upload a photo")
+        else:
+            # print(description)
+            # print(EMAIL)
+            print(memory.filename)
+    return redirect("/upload")
+
+@app.route('/redescribe')
+def redescribe():
+    # for memory in image:
+    #     if len(memory["new sentences"]) == 0:
+    #         return render_template('redescribe.html', errorMessage="", image=url_for('file', filename=memory["file"]))
+    return render_template('redescribe.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
